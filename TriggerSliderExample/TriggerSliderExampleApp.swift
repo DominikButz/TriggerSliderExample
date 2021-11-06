392a1000c6f9b9fbe2c062cc3d3720ee83197bc1
Dominik <dominikbutz@gmail.com>
2021-11-06 17:22:22 +0800

    Initial Commit

---
+++ TriggerSliderExample/TriggerSliderExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriggerSliderExampleApp.swift
+//  TriggerSliderExample
+//
+//  Created by Dominik Butz on 6/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct TriggerSliderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
